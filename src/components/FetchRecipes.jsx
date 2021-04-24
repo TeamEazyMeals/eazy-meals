3cda5720abf09c7f77e935806a4338020d2f740d
Loreen-netizen <loreenmuusha@gmail.com>
2021-04-24 21:14:02 +0200

    recipe data showing in console

--- src/components/FetchRecipes.jsx
+++ src/components/FetchRecipes.jsx
@@ -1,33 +1,37 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 const FetchRecipes = () => {
-
-const [recipeData, setRecipeData] = useState(null);
+  const [recipeData, setRecipeData] = useState(null);
 
   const getRecipeData = async () => {
-    const response = await fetch("/data/RecipeData.json");
+    const response = await fetch("/data/recipeData.json");
     const data = await response.json();
     console.log(data);
+    //  alert(data);
     setRecipeData(data);
   };
 
-    const useEffect = (() => getRecipeData(),[]);
-
-if(!recipeData){
-    return <h1> Loading.....</h1>
-}else{
-    return(
-        recipeData.map((recipe)=>{
+   useEffect (() => getRecipeData(), []);
 
 
-<ul>
-  <li>{recipe.name}</li>
-  <li>{recipe.image}</li>
-  <li>{recipe.ingredients}</li>
-  <li>{recipe.description}</li>
-</ul>;
-    })
-    )}
-    
+  if (!recipeData) {
+    return <h1> Loading.....</h1>;
+  } 
+    return (
+      
+      <div>
+        <h1>RECIPES</h1>  
+        {
+        recipeData.map((recipe) => {
+          <ul>
+            <li key={recipe.id}>{recipe.name}</li>
+            <li key={recipe.id}>{recipe.image}</li>
+            <li key={recipe.id}>{recipe.ingredients}</li>
+            <li key={recipe.id}>{recipe.description}</li>
+          </ul>;
+        })}
+      </div>
+    );
+  
 };
 export default FetchRecipes;
