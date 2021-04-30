5bba5dff92e5ac73af3a1f0c929b4f62df1cbcc4
Loreen-netizen <loreenmuusha@gmail.com>
2021-04-30 23:00:02 +0200

    search now filtering ascending and descending alphabetically, by ascending and descending time in minutes

--- src/components/SortRecipes/SortRecipes.jsx
+++ src/components/SortRecipes/SortRecipes.jsx
@@ -1,21 +1,18 @@
-
-
-
 const Sort = (props) => {
-    const {handleSort} = props;
+  const { handleSort } = props;
   return (
     <div>
       <select defaultValue="sort" onChange={handleSort}>
         <option value="sort" disabled>
           Sort Recipes
         </option>
+        <option value="a-z">a-z</option>
+        <option value="z-a">z-a</option>
         <option value="shortest time">shortest time</option>
         <option value="longest time">longest time</option>
-        <option value="alphabetically">alphabetically</option>
       </select>
     </div>
   );
- 
 };
 
 export default Sort;
