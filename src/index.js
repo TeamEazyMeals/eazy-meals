7a3be6fe4af25c9b9726269ec4d18f86a0949a76
Loreen-netizen <loreenmuusha@gmail.com>
2021-05-12 10:35:38 +0200

    import ReactDOM from "react-dom"; fixed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App/App";
-import reportWebVitals from "./reportWebVitals";
 
-ReactDom.render(
+ReactDOM.render(
   <App />,  
   document.getElementById('root')   
 );
