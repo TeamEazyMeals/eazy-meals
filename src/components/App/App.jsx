0680ef537a075be295d7de329dc9d8dd1b78c500
Yonela <yoza.futho@gmail.com>
2021-05-12 10:36:15 +0200

    Added the home page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
         <Route path="/forgotpassword" children={<ForgotPassword></ForgotPassword>}/>
         <Route path="/" children={<SignUp></SignUp>} />
       </Switch>
+      <App />
     </BrowserRouter>
   );
   
