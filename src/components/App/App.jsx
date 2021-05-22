ee0dc917fdfb0ba98597beedc9bebeb21a92fca6
ptawuringa <ptaruwinga@yahoo.com>
2021-05-22 14:49:19 +0200

    config setup

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,17 +10,17 @@ import Health from "../../views/Health";
 import Preferences from "../../views/Preferences";
 import Goals from "../../views/Goals";
 
+import HomePage from "../../views/AllRecipes/HomePage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
-
-      <Route path="/config/allergies" children={<Allergies />} />
-      <Route path="/config/health" children={<Health />} />
-      <Route path="/config/preferences" children={<Preferences/>} />
-      <Route path="/config/goals" children={<Goals />} />
-      <Route path="/config" children={<Config />} />
+        <Route path="/config/allergies" children={<Allergies />} />
+        <Route path="/config/health" children={<Health />} />
+        <Route path="/config/preferences" children={<Preferences />} />
+        <Route path="/config/goals" children={<Goals />} />
+        <Route path="/config" children={<Config />} />
         <Route path="/allrecipes/getrecipe" children={<GetRecipe />} />
         <Route path="/allrecipes/:recipeId" children={<GetRecipe />} />
         <Route path="/allrecipes" children={<AllRecipes />} />
