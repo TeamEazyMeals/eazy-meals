74b7ee708f42f364731329defc561bd5af8e43a7
Loreen-netizen <loreenmuusha@gmail.com>
2021-05-26 17:07:02 +0200

    upload recipes pages layout created

---
+++ src/components/UploadCustomRecipe/UploadCustomRecipe.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const  UploadCustomRecipe = ()=>{
+    return (
+      <div>
+        <h1> Upload Custom Recipe</h1>
+        <input type="text" placeholder="recipe name"></input>
+        <h3>UPLOAD RECIPE PHOTO</h3>
+      </div>
+    );
+}
+
+export default  UploadCustomRecipe;
