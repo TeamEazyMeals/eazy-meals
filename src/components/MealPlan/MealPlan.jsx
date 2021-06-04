21c8eb8c0056683f328f9b5390f697f764d6e8f1
Yonela <yoza.futho@gmail.com>
2021-06-04 11:49:14 +0200

    Adding style

--- src/components/MealPlan/MealPlan.jsx
+++ src/components/MealPlan/MealPlan.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import Dropdown from "react-dropdown";
 import styled from "styled-components";
-import { Button } from "@material-ui/core"
- 
+import { Button } from "@material-ui/core";
+
 const Header = styled.h1`
   font-size: 64px;
   text-align: center;
@@ -10,7 +10,33 @@ const Header = styled.h1`
   font: Robot;
   background: #34a853;
 `;
+const Container = styled.h2`
+  text-align: center;
+  font: Robot;
+`;
+const Form = styled.label`
+  top-padding: 8rem;
+  top-margin: 9rem;
+  botton-nargin: 4rem;
+  text-align: center;
+`;
 
+const StyledButton = styled(Button)`
+  && {
+    top-padding: 50px;
+    height: 40px;
+    width: 250px;
+    border: 1px solid #34a853;
+    color: #34a853;
+    font-weight: 700;
+    border-radius: 3px;
+    float: center;
+  }
+`;
+
+const Wrapper = styled.div`
+
+`;
 
 
 const MealPlan = () => {
@@ -28,50 +54,55 @@ const MealPlan = () => {
 
   return (
     <div>
-      <h1>Select Meal Plan</h1>
-      <h2>Welcome Busi...</h2>
-      <p> Please select your meal plan</p>
-      <form>
-        <label for="days">Please select a day:</label>
+      <Header>Select Meal Plan</Header>
+      <Container>
+        <h2>Welcome Busi...</h2>
+        <p> Please select your meal plan</p>
+      </Container>
 
-        <select id="dropdown" onChange={handleDaySelect}>
-        <option value=""></option>
-          <option value="Sunday">Sunday</option>
-          <option value="Monday">Monday</option>
-          <option value="Tuesday">Tuesday</option>
-          <option value="Wednesday">Wednesday</option>
-          <option value="Thursday">Thursday</option>
-          <option value="Friday">Friday</option>
-          <option value="Saturday">Saturday</option>
-        </select>
+      <Form>
+        <label for="days">
+          Please select a day:
+          <select id="dropdown" onChange={handleDaySelect}>
+            <option value=""></option>
+            <option value="Sunday">Sunday</option>
+            <option value="Monday">Monday</option>
+            <option value="Tuesday">Tuesday</option>
+            <option value="Wednesday">Wednesday</option>
+            <option value="Thursday">Thursday</option>
+            <option value="Friday">Friday</option>
+            <option value="Saturday">Saturday</option>
+          </select>
+        </label>
         <br />
+        <br />
+        <label for="days">
+          Please select a Type:
+          <select id="dropdown" onChange={handleMealTypeSelect}>
+            <option value=""></option>
+            <option value="Breakfast">Breakfast</option>
+            <option value="Lunch">Lunch</option>
+            <option value="Dinner">Dinner</option>
+            <option value="Snack">Snack</option>
+          </select>
+        </label>
 
-        <label for="days">Please select a Type:</label>
-        <select id="dropdown" onChange={handleMealTypeSelect}>
-        <option value=""></option>
-          <option value="Breakfast">Breakfast</option>
-          <option value="Lunch">Lunch</option>
-          <option value="Dinner">Dinner</option>
-          <option value="Snack">Snack</option>
-        </select>
-
-      
         {mealTypeValue === "" ? (
-        <b></b>
-      ) : dayValue !== "" ? (
-        <div>
-          <b>Selected Meal Type: {mealTypeValue}</b> <br></br>
-          <b>Selected Day:{dayValue}</b>
-        </div>
-      ) : (
-        <p>Please select Meal Type and Day.</p>
-      )}
-      
-      </form>
-      <Button variant="contained" href="/allrecipes">
-            Submit
-          </Button>
-
+          <b></b>
+        ) : dayValue !== "" ? (
+          <Wrapper>
+          <div>
+            <b>Selected Meal Type: {mealTypeValue}</b> <br></br>
+            <b>Selected Day:{dayValue}</b>
+          </div>
+          </Wrapper>
+        ) : (
+          <p>Please select Meal Type and Day.</p>
+        )}
+      </Form>
+      <StyledButton variant="contained" href="/allrecipes">
+        Submit
+      </StyledButton>
     </div>
   );
 };
