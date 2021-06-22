b359af57f16e0106e9949cb892087f9323d0cf06
Loreen-netizen <loreenmuusha@gmail.com>
2021-06-22 17:41:34 +0200

    data store not yet updating

--- scripts/pullGraphCmsData.mjs
+++ scripts/pullGraphCmsData.mjs
@@ -1,8 +1,6 @@
-
-
 import axios from "axios";
 import fs from "fs/promises";
-import md5 from "md5"
+import md5 from "md5";
 const GET_ALL_RECIPE_DATA_QUERY = `
 {
   recipes {
@@ -14,12 +12,10 @@ const GET_ALL_RECIPE_DATA_QUERY = `
       url
     }
     ingredients
-
     steps
   }
 }
 `;
-
 const init = async () => {
   const {
     data: {
@@ -27,24 +23,33 @@ const init = async () => {
     },
   } = await axios.post(
     "https://api-eu-central-1.graphcms.com/v2/cko2w2ux95cw901z18eesaeu8/master",
-    { query: GET_ALL_RECIPE_DATA_QUERY }
+    {
+      query: GET_ALL_RECIPE_DATA_QUERY,
+    }
   );
-
-  const recipePromiseArray = recipes.map(async (singleRecipe) => {
-   const stringVersion = JSON.stringify(singleRecipe);
+  const promiseArray = recipes.map(async (singleRecipe) => {
+    const stringVersion = JSON.stringify(singleRecipe);
     await fs.writeFile(
       `./public/data/recipes/${singleRecipe.id}.json`,
       stringVersion
     );
-    return md5(stringVersion)
+    return [singleRecipe.id, md5(stringVersion)];
   });
-  const hashes = await Promise.all(recipePromiseArray).catch(console.error);
+  const hashes = await Promise.all(promiseArray).catch(console.error);
   const primaryHash = md5(hashes);
+  const recipeHashes = hashes.reduce(
+    (result, [id, hash]) => ({
+      ...result,
+      [id]: hash,
+    }),
+    {}
+  );
   await fs.writeFile(
-    `./public/data/recipe-hashes.json`,
+    "./public/data/recipe-hashes.json",
     JSON.stringify({
-      primary:primaryHash,
-      array:hashes,
+      id: "hashes",
+      primary: primaryHash,
+      recipes: recipeHashes,
     })
   );
 };
