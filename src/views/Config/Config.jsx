77161885a162bc9d9b59a309a1830259cfc92532
ptawuringa <ptaruwinga@yahoo.com>
2021-07-22 13:34:47 +0200

    new changes on config

--- src/views/Config/Config.jsx
+++ src/views/Config/Config.jsx
@@ -1,331 +1,307 @@
- import React from "react";
+import React, { useState, useEffect } from "react";
 
- const Config =()=>{
-   return (
-     <div>123</div>
-   )
- }
- export default Config;
+import { Button, Checkbox } from "@material-ui/core";
+import styled from "styled-components";
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
+const CONFIG_MAP = {
+  allergies: {
+    id: "allergies",
+    title: "Allergies",
+    options: {
+      null: {
+        id: "null",
+        title: "No Allergies",
+        reset: true,
+      },
+      fish: {
+        id: "fish",
+        title: "Fish",
+      },
+      gluten: {
+        id: "gluten",
+        title: "Gluten",
+      },
+      dairy: {
+        id: "dairy",
+        title: "Dairy",
+      },
+      nuts: {
+        id: "nuts",
+        title: "Nuts",
+      },
+    },
+  },
+  healthConditions: {
+    id: "healthConditions",
+    title: "Health Conditions",
+    options: {
+      null: {
+        id: "null",
+        title: "No Health Conditions",
+        reset: true,
+      },
 
+      diabetes: {
+        id: "diabetes",
+        title: "Diabetes",
+      },
+      cancer: {
+        id: "cancer",
+        title: "Cancer",
+      },
+      "heart-conditions": {
+        id: "heart-conditions",
+        title: "Heart Conditions",
+      },
+    },
+  },
+  preferences: {
+    id: "preferences",
+    title: "Preferences",
+    options: {
+      null: {
+        id: "null",
+        title: "No Preferences",
+        reset: true,
+      },
+      "low-carb": {
+        id: "low-carb",
+        title: "Low-Carb",
+      },
+      vegan: {
+        id: "vegan",
+        title: "Vegan",
+      },
+      keto: {
+        id: "keto",
+        title: "Keto",
+      },
+      pescatarian: {
+        id: "pescatarian",
+        title: "Pescatarian",
+      },
+    },
+  },
+  goals: {
+    id: "goals",
+    title: "Goals",
+    options: {
+      null: {
+        id: "null",
+        title: "No Goals",
+        reset: true,
+      },
+      "lose-weight": {
+        id: "lose-weight",
+        title: "Lose-Weight",
+      },
+      "health-eating": {
+        id: "health-eating",
+        title: "Health-Eating",
+      },
+      "gain-weight": {
+        id: "gain-weight",
+        title: "Gain-weight",
+      },
+    },
+  },
+};
 
+const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+/* Styled Components*/
+const ButtonWrap = styled.div`
+  padding: 0.5rem;
+`;
+/* Helper functions */
 
+const createIntialObj = (obj) =>
+  Object.keys(obj.options).reduce(
+    (result, key) => ({
+      ...result,
+      [key]: false,
+    }),
+    {}
+  );
 
+const isValidSelections = (obj) => {
+  if (obj.none) return true;
 
+  const allSelections = Object.values(obj);
+  const trueSelections = allSelections.filter((value) => value === true);
+  return trueSelections.length > 0;
+};
 
+const createAction = (stateObj, setter) => (id, reset) => {
+  if (stateObj.none && !reset) {
+    setter({
+      ...stateObj,
+      [id]: !stateObj[id],
+      none: false,
+    });
+    return;
+  }
 
+  if (!stateObj.none && reset) {
+    setter({
+      ...Object.values(stateObj).reduce(
+        (result, key) => ({
+          ...result,
+          [key]: false,
+        }),
+        {}
+      ),
+      [id]: !stateObj[id],
+      none: true,
+    });
+    return;
+  }
 
-//import React, { useState, useEffect } from "react";
- 
+  setter({
+    ...stateObj,
+    [id]: !stateObj[id],
+    none: false,
+  });
+};
 
-// import { Button, Checkbox } from "@material-ui/core";
-// import styled from "styled-components";
+/*Values*/
 
-// const Wrapper = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-// `;
+const localString = window.localStorage.getItem("config");
 
-// const CONFIG_MAP = {
-//   allergies: {
-//     id: "allergies",
-//     title: "Allergies",
-//     options: {
-//       null: {
-//         id: "null",
-//         title: "No Allergies",
-//         reset: true,
-//       },
-//       fish: {
-//         id: "fish",
-//         title: "Fish",
-//       },
-//       gluten: {
-//         id: "gluten",
-//         title: "Gluten",
-//       },
-//       dairy: {
-//         id: "dairy",
-//         title: "Dairy",
-//       },
-//       nuts: {
-//         id: "nuts",
-//         title: "Nuts",
-//       },
-//     },
-//   },
-//   healthConditions: {
-//     id: "healthConditions",
-//     title: "Health Conditions",
-//     options: {
-//       null: {
-//         id: "null",
-//         title: "No Health Conditions",
-//         reset: true,
-//       },
+const createBlankValues = () => ({
+  allergies: createIntialObj(CONFIG_MAP.allergies),
+  healthConditions: createIntialObj(CONFIG_MAP.healthConditions),
+  preferences: createIntialObj(CONFIG_MAP.preferences),
+  goals: createIntialObj(CONFIG_MAP.goals),
+});
 
-//       diabetes: {
-//         id: "diabetes",
-//         title: "Diabetes",
-//       },
-//       cancer: {
-//         id: "cancer",
-//         title: "Cancer",
-//       },
-//       "heart-conditions": {
-//         id: "heart-conditions",
-//         title: "Heart Conditions",
-//       },
-//     },
-//   },
-//   preferences: {
-//     id: "preferences",
-//     title: "Preferences",
-//     options: {
-//       null: {
-//         id: "null",
-//         title: "No Preferences",
-//         reset: true,
-//       },
-//       "low-carb": {
-//         id: "low-carb",
-//         title: "Low-Carb",
-//       },
-//       vegan: {
-//         id: "vegan",
-//         title: "Vegan",
-//       },
-//       keto: {
-//         id: "keto",
-//         title: "Keto",
-//       },
-//       pescatarian: {
-//         id: "pescatarian",
-//         title: "Pescatarian",
-//       },
-//     },
-//   },
-//   goals: {
-//     id: "goals",
-//     title: "Goals",
-//     options: {
-//       null: {
-//         id: "null",
-//         title: "No Goals",
-//         reset: true,
-//       },
-//       "lose-weight": {
-//         id: "lose-weight",
-//         title: "Lose-Weight",
-//       },
-//       "health-eating": {
-//         id: "health-eating",
-//         title: "Health-Eating",
-//       },
-//       "gain-weight": {
-//         id: "gain-weight",
-//         title: "Gain-weight",
-//       },
-//     },
-//   },
-// };
+const initialValues = localString
+  ? JSON.parse(localString)
+  : createBlankValues();
 
-// const InputWrapper = styled.div`
-//   display: flex;
-//   flex-direction: row;
-//   align-items: center;
-// `;
-// /* Styled Components*/
-// const ButtonWrap = styled.div`
-//   padding: 0.5rem;
-// `;
-// /* Helper functions */
+const Config = () => {
+  const [open, setOpen] = useState(null);
 
-// const createIntialObj = (obj) =>
-//   Object.keys(obj.options).reduce(
-//     (result, key) => ({
-//       ...result,
-//       [key]: false,
-//     }),
-//     {}
-//   );
+  const [allergies, setAllergies] = useState(initialValues.allergies);
+  const [healthConditions, setHealthConditions] = useState(
+    initialValues.healthConditions
+  );
+  const [preferences, setPreferences] = useState(initialValues.preferences);
+  const [goals, setGoals] = useState(initialValues.goals);
 
-// const isValidSelections = (obj) => {
-//   if (obj.none) return true;
+  const selections = {
+    allergies,
+    preferences,
+    goals,
+    healthConditions,
+  };
 
-//   const allSelections = Object.values(obj);
-//   const trueSelections = allSelections.filter((value) => value === true);
-//   return trueSelections.length > 0;
-// };
+  useEffect(() => {
+    window.localStorage.setItem("config", JSON.stringify(selections));
+  }, [allergies, preferences, goals, healthConditions]);
 
-// const createAction = (stateObj, setter) => (id, reset) => {
+  const actions = {
+    allergies: createAction(allergies, setAllergies),
 
-//   if (stateObj.none && !reset) {
-//     setter({
-//       ...stateObj,
-//       [id]: !stateObj[id],
-//       none: false,
-//     });
-//     return;
-//   }
+    preferences: createAction(preferences, setPreferences),
 
+    goals: createAction(goals, setGoals),
 
-//    if (!stateObj.none && reset) {
+    healthConditons: createAction(healthConditions, setHealthConditions),
+  };
 
-//      setter({
-//        ...Object.values(stateObj).reduce(
-//          (result, key) => ({
-//            ...result,
-//           [key]: false,
-//         }),
-//       {}
-//       ),
-//        [id]: !stateObj[id],
-//        none: true,
-//      });
-//      return;
-//     }
+  const validSelections = {
+    allergies: isValidSelections(allergies),
+    healthConditions: isValidSelections(healthConditions),
+    preferences: isValidSelections(preferences),
+    goals: isValidSelections(goals),
+  };
 
+  if (open) {
+    const resets = Object.values(CONFIG_MAP[open].options).filter(
+      (item) => item.reset === true
+    );
 
-//   setter({
-//     ...stateObj,
-//     [id]: !stateObj[id],
-//     none: false,
-//   });
-// }
+    const nonResets = Object.values(CONFIG_MAP[open].options).filter(
+      (item) => item.reset !== true
+    );
 
-//   /*Values*/
+    return (
+      <div>
+        <h1>{CONFIG_MAP[open].title}</h1>
 
-//   const localString = window.localStorage.getItem("config");
+        {resets.map(({ id, title }) => (
+          <div key={id}>
+            <label htmlFor={id}>
+              <Checkbox
+                id={id}
+                checked={Boolean(selections[open][id])}
+                onClick={() => actions[open](id, true)}
+              />
+              <span>{title}</span>
+            </label>
+          </div>
+        ))}
 
-//   const createBlankValues = () => ({
-//     allergies: createIntialObj(CONFIG_MAP.allergies),
-//     healthConditions: createIntialObj(CONFIG_MAP.healthConditions),
-//     preferences: createIntialObj(CONFIG_MAP.preferences),
-//     goals: createIntialObj(CONFIG_MAP.goals),
-//   });
+        <hr />
 
-//   const initialValues = localString
-//     ? JSON.parse(localString)
-//     : createBlankValues();
+        {nonResets.map(({ id, title }) => (
+          <div key={id}>
+            <label htmlFor={id}>
+              <Checkbox
+                id={id}
+                checked={Boolean(selections[open][id])}
+                onClick={() => actions[open](id)}
+              />
+              <span>{title}</span>
+            </label>
+          </div>
+        ))}
 
-//   const Config = () => {
-//     const [open, setOpen] = useState(null);
+        <Button variant="contained" onClick={() => setOpen(null)}>
+          Continue
+        </Button>
+      </div>
+    );
+  }
 
-//     const [allergies, setAllergies] = useState(initialValues.allergies);
-//     const [healthConditions, setHealthConditions] = useState(
-//       initialValues.healthConditions
-//     );
-//     const [preferences, setPreferences] = useState(initialValues.preferences);
-//     const [goals, setGoals] = useState(initialValues.goals);
+  return (
+    <div>
+      <h1>Config</h1>
 
-//     const selections = {
-//       allergies,
-//       preferences,
-//       goals,
-//       healthConditions,
-//     };
+      {Object.values(CONFIG_MAP).map(({ id, title }) => (
+        <ButtonWrap key={id}>
+          {" "}
+          <Checkbox
+            checked={Boolean(selections[open][id])}
+            onChange={console.log}
+          />
+          <Button variant="contained" onClick={() => setOpen(id)}>
+            Add {title}
+          </Button>
+        </ButtonWrap>
+      ))}
 
-//     useEffect(() => {
-//       window.localStorage.setItem("config", JSON.stringify(selections));
-//     }, [allergies, preferences, goals, healthConditions]);
+      <Button
+        variant="contained"
+        disabled={
+          Object.values(validSelections).filter((value) => value === false)
+            .length > 0
+        }
+      >
+        Save Config
+      </Button>
+    </div>
+  );
+};
 
-//     const actions = {
-//       allergies: createAction(allergies, setAllergies),
-
-//       preferences: createAction(preferences, setPreferences),
-
-//       goals: createAction(goals, setGoals),
-
-//       healthConditons: createAction(healthConditions, setHealthConditions),
-//     };
-
-//     const validSelections = {
-//       allergies: isValidSelections(allergies),
-//       healthConditions: isValidSelections(healthConditions),
-//       preferences: isValidSelections(preferences),
-//       goals: isValidSelections(goals),
-//     };
-
-//     if (open) {
-//       const resets = Object.values(CONFIG_MAP[open].options).filter(
-//         (item) => item.reset === true
-//       );
-
-//       const nonResets = Object.values(CONFIG_MAP[open].options).filter(
-//         (item) => item.reset !== true
-//       );
-
-//       return (
-//         <div>
-//           <h1>{CONFIG_MAP[open].title}</h1>
-
-//           {resets.map(({ id, title }) => (
-//             <div key={id}>
-//               <label htmlFor={id}>
-//                 <Checkbox
-//                   id={id}
-//                   checked={Boolean(selections[open][id])}
-//                   onClick={() => actions[open](id, true)}
-//                 />
-//                 <span>{title}</span>
-//               </label>
-//             </div>
-//           ))}
-
-//           <hr />
-
-//           {nonResets.map(({ id, title }) => (
-//             <div key={id}>
-//               <label htmlFor={id}>
-//                 <Checkbox
-//                   id={id}
-//                   checked={Boolean(selections[open][id])}
-//                   onClick={() => actions[open](id)}
-//                 />
-//                 <span>{title}</span>
-//               </label>
-//             </div>
-//           ))}
-
-//           <Button variant="contained" onClick={() => setOpen(null)}>
-//             Continue
-//           </Button>
-//         </div>
-//       );
-//     }
-
-//     return (
-//       <div>
-//         <h1>Config</h1>
-
-//         {Object.values(CONFIG_MAP).map(({ id, title }) => (
-//           <ButtonWrap key={id}>
-//             <Checkbox
-//               checked={Boolean(selections[open][id])}
-//               onChange={console.log}
-            
-//             />
-
-//             <Button variant="contained" onClick={() => setOpen(id)}>
-//               Add {title}
-//             </Button>
-//           </ButtonWrap>
-//         ))}
-
-//         <Button
-//           variant="contained"
-//           disabled={
-//             Object.values(validSelections).filter((value) => value === false)
-//               .length > 0
-//           }>
-//           Save Config
-//         </Button>
-//       </div>
-//     );
-//   };
-
-
-// export default Config;
+export default Config;
